215f590c907bd16786bc95b2bec0c4510b107e3e
Liam Brooke <24947193+ItsLimaBean@users.noreply.github.com>
2022-11-28 14:37:17 -0800

    Added random tile generation.

--- game/breakout/components/tilegroup.py
+++ game/breakout/components/tilegroup.py
@@ -1,12 +1,23 @@
+import math
 import pygame
 from .tile import Tile
+import random
 
 
 class TileGroup(pygame.sprite.Group):
     def __init__(self, tile_width=100, tile_height=30):
         super().__init__()
 
-        tile = Tile(width=tile_width, height=tile_height)
+        self.tiles = []
+        self.tile_count = random.randint(50, 100)
+        for i in range(self.tile_count):
+            x = (i % 8) * tile_width 
+            y = math.floor(i / 8) * tile_height
+            if random.randint(0, 10) != 3:
+                tile = Tile(width=tile_width, height=tile_height)
 
-        tile.move_to(400, 400)
-        self.add(tile)
+                tile.move_to(x, y)
+
+                self.tiles.append(tile)
+
+        self.add(self.tiles)
